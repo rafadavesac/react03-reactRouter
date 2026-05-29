0fd5d9396bd62d39ab13751e705478f29feb856b
rafadavesac <rafalaimerdavesac@gmail.com>
2026-05-29 23:42:00 +0000

    parametro de id dos posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,17 +1,19 @@
 import PostItem from "./PostItem";
+import { Link } from "react-router-dom";
 
-export default function PostList({ posts, onPostClick }) {
+export default function PostList({ posts }) {
   return (
     <div className="flex flex-col gap-4">
       {posts.map((post) => (
+        <Link to={`/post/${post.id}`} key={post.id} className="block">
         <PostItem
           key={post.id}
           title={post.title}
           summary={post.summary}
           author={post.author}
           date={post.date}
-          onClick={() => onPostClick(post.id)}
         />
+        </Link>
       ))}
     </div>
   );
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,16 +1,30 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { useParams } from "react-router-dom";
 
 // importar os dados dos posts
 import posts from "../data/posts";
 
 function Post() {
+  const { id } = useParams();
+  const post = posts.find((p) => p.id === parseInt(id));
+
+  if(!post) {
+    return (
+      <>
+        <Header title="Meu Blog" />
+        <h1>Post não encontrado</h1>
+        <Footer text="Todos os direitos reservados" />
+      </>
+    );
+  }
+
     return (
         <>
             <Header title="Meu Blog" />
-            <h1>{posts[0].title}</h1>
-            <p>{posts[0].author} / {posts[0].date}</p>
-            <p>{posts[0].content}</p>
+            <h1>{post.title}</h1>
+            <p>{post.author} / {post.date}</p>
+            <p>{post.content}</p>
             <Footer text="Todos os direitos reservados" />
         </>
     );
